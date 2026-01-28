1c844e286e9b604ddbade1636a31a7830d02f4d2
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-01-28 10:23:46 +0530

    multer setup done

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -16,6 +16,10 @@ const userSchema=new mongoose.Schema({
         type:String,
         default:""
 
+    },
+    channel:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Channel"
     }
    
 
