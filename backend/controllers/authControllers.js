ac433d0c4b66f9a7b5afba7ec1614d03baa65720
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-01-29 00:02:40 +0530

    login controller done

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -51,3 +51,33 @@ export const signUp=async(req,res)=>{
         return res.status(500).json({message:"Internal server error"});
     }
 }
+export const signIn=async(req,res)=>{
+    try {
+        const {email,password}=req.body;
+        if(!email || !password){
+            return res.status(400).json({message:"All fields are required"});
+        }
+        const user=await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message:"User not found"})
+        }
+        const isPasswordMatched=await bcrypt.compare(password,
+            user.password
+        )
+if(!isPasswordMatched){
+    return res.status(400).json({message:"Invalid credentials"})
+}
+let token=await gentoken(user._id);
+res.cookie("token",token,{
+    httpOnly:true,
+    secure:false,
+    sameSite:"strict",
+    maxAge:7*24*60*60*1000
+})
+return res.status(200).json({message:"User logged in successfully",user,token})
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message:"Internal server error"})
+    }
+}
