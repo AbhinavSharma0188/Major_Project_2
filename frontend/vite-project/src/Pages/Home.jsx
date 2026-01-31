ef5fc74aa8cb07add8165493cf5031862c4c1f19
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-01-31 23:01:41 +0530

    created custom hook component

--- frontend/vite-project/src/Pages/Home.jsx
+++ frontend/vite-project/src/Pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import CustomAlert from '../component/CustomAlert'
 function Home() {
   return (
     <div className='text-red-600 text-4xl'>
-        Home Page
+       <CustomAlert/>
         
         </div>
   )
---
+++ frontend/vite-project/src/component/CustomAlert.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CustomAlert() {
+  return (
+    <div className='fixed inset-0 flex items-start justify-center pt-[50px] bg-black/50 z-50'></div>
+  )
+}
+
+export default CustomAlert
