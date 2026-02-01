144ff5327854926843604285df9db244ffe0cc39
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-02-02 00:31:00 +0530

    left designing of home.jsx done

--- frontend/vite-project/src/Pages/Home.jsx
+++ frontend/vite-project/src/Pages/Home.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import CustomAlert from '../component/CustomAlert'
+import { FaBars } from "react-icons/fa";
+import logo from '../../public/logo.png'
+import { FaSearch } from "react-icons/fa";
 function Home() {
   return (
-    <div className='text-red-600 text-4xl'>
-      HomePage
+    <div className='bg-[#0f0f0f] text-white min-h-screen relative'>
+      {/* {navbar} */}
+
+      <header className='bg-[#0f0f0f] h-15 p-3 border-b border-gray-800 fixed top-0 left-0 right-0 z-50' >
+
+
+        <div className='flex items-center justify-between'>
+          {/* {left} */}
+          <div className='flex items-center gap-4'>
+            <button className='text-xl bg-[#272727] p-2 rounded-full md:inline hidden'><FaBars /></button>
+            <div className='flex items-center gap-[5px]'>
+              <img src={logo} alt=""  className='w-[30px]'/>
+              <span className='text-white font-bold text-xl tracking-tight font-roboto'>Flow-Forge</span>
+
+
+
+            </div>
+
+          </div>
+          {/* {search} */}
+          <div className='hidden md:flex items-center gap-2 flex-1 max-w-xl'>
+
+            <div>
+              <input type="" className='flex-1 bg-[#121212] px-4 py-2 rounded-1-full outline-none border border-gray-700' placeholder='Search'/>
+              <button className='bg-[#272727] px-4 py-2 rounded-1-full outline-none border border-gray-700'><FaSearch /></button>
+            </div>
+          </div>
+        </div>
+      </header>
+    
        
        
         
