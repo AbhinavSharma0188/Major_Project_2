bd801f4627f26c53f071970a2fa596188d578bdd
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-02-05 00:11:54 +0530

    sidebar hamburger onclick functionality done

--- frontend/vite-project/src/Pages/Home.jsx
+++ frontend/vite-project/src/Pages/Home.jsx
@@ -1,6 +1,10 @@
 import { FaBars, FaMicrophone, FaSearch, FaUserCircle } from "react-icons/fa";
 import logo from '../../public/logo.png';
+import { useState } from "react";
 function Home() {
+  const [sidebarOpen,setSidebarOpen]=useState(true)
+  const [selectedItem,setSelectedItem]=useState("Home")
+
   return (
     <div className='bg-[#0f0f0f] text-white min-h-screen relative'>
       {/* {navbar} */}
@@ -11,7 +15,8 @@ function Home() {
         <div className='flex items-center justify-between'>
           {/* {left} */}
           <div className='flex items-center gap-4'>
-            <button className='text-xl bg-[#272727] p-2 rounded-full md:inline hidden'><FaBars /></button>
+            <button className='text-xl bg-[#272727] p-2 rounded-full md:inline hidden' onClick={()=>setSidebarOpen(!sidebarOpen)
+             }><FaBars /></button>
             <div className='flex items-center gap-[5px]'>
               <img src={logo} alt="" className='w-[30px]' />
               <span className='text-white font-bold text-xl tracking-tight font-roboto'>Flow-Forge</span>
@@ -46,6 +51,12 @@ function Home() {
         </div>
       </header>
 
+{/* sidebar */}
+<aside  className={`bg-[#0f0f0f] border-r border-gray-800 transition-all duration-300 fixed top-[60px] bottom-0 z-40
+          ${sidebarOpen ? "w-60" : "w-20"} hidden md:flex flex-col overflow-y-auto`}
+      >
+
+</aside>
 
 
 
@@ -54,7 +65,13 @@ function Home() {
 }
 function sidebarItem({ icon, text, open, selected, onClick }) {
   return (
-    <button></button>
+    <button
+      onClick={onClick}
+      className={`flex items-center gap-4 p-2 rounded w-full transition-colors ${open ? "justify-start" : "justify-center"} ${selected ? "bg-[#272727]" : "hover:bg-[#272727]"}`}
+    >
+      <span className="text-lg">{icon}</span>
+      {open && <span className="text-sm">{text}</span>}
+    </button>
 
   )
 }
